3c3348e9d0ebeb55e4973fde5cefbaef98869505
Varsha8738 <sreevarshapenta@gmail.com>
2025-04-09 21:53:49 +0530

    Create first_fit.c

---
+++ first_fit.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+void firstFit(int blockSize[], int m, int processSize[], int n) {
+    int allocation[n];
+    for (int i = 0; i < n; i++)
+        allocation[i] = -1;
+    for (int i = 0; i < n; i++) {
+        int firstIdx = -1;
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                firstIdx = j;
+                break;
+            }
+        }
+        if (firstIdx != -1) {
+            allocation[i] = firstIdx;
+            blockSize[firstIdx] -= processSize[i];
+        }
+    }
+    printf("Process No.\tProcess Size\tBlock No.\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t\t%d\t\t", i, processSize[i]);
+        if (allocation[i] != -1)
+            printf("%d\n", allocation[i]);
+        else
+            printf("Not Allocated\n");
+    }
+}
+int main() {
+    int i, bs, p, blockSize[10], processSize[10];
+    printf("Enter no. of blocks: ");
+    scanf("%d", &bs);
+    for (i = 0; i < bs; i++) {
+        printf("Enter %d block size: ", i);
+        scanf("%d", &blockSize[i]);
+    }
+    printf("Enter no. of processes: ");
+    scanf("%d", &p);
+    for (i = 0; i < p; i++) {
+        printf("Enter %d process size: ", i);
+        scanf("%d", &processSize[i]);
+    }
+    firstFit(blockSize, bs, processSize, p);
+    return 0;
+}
+OUTPUT:
+Enter no. of blocks: 3
+Enter 0 block size: 1
+Enter 1 block size: 2
+Enter 2 block size: 3
+Enter no. of processes: 3
+Enter 0 process size: 1
+Enter 1 process size: 2
+Enter 2 process size: 3
+Process No.     Process Size    Block No.
+0               1               0
+1               2               1
+2               3               2
